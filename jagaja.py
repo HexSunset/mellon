d1f0e50951d0bd9eac205b6b2ee0f0746bffaff9
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-04-19 23:37:02 +0300

    Sisendi tükkideks jagaja Iterator class
    Jagab baidid laiali n-biti suurusteks tükkideks, annab Iterator-i
    nende kasutamiseks.

---
+++ jagaja.py
@@ -0,0 +1,24 @@
+from bitarray import bitarray
+
+class Jagaja:
+    # TODO: toeta erinevaid sisendi variante?
+    def __init__(self, sisend: bytes, tüki_suurus=2):
+        self.tüki_suurus: int = tüki_suurus
+        self.baidid = bitarray()
+        self.baidid.frombytes(sisend)
+        self.indeks = 0
+        self.pikkus = len(self.baidid)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.indeks < self.pikkus:
+            raise StopIteration
+
+        algus = self.indeks
+        lõpp = self.indeks + self.tüki_suurus
+        tükk = self.baidid[algus:lõpp]
+        #print(f"algus = {algus}; lõpp = {lõpp}; tükk = {tükk.to01()}")
+        self.indeks += self.tüki_suurus
+        return tükk
