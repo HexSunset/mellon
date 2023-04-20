1db9a22c187c1030a9dca7915758ed163a965b54
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-04-21 00:19:12 +0300

    Kodeerija klassi algus, init funktsioon

---
+++ kodeerija.py
@@ -0,0 +1,36 @@
+from PIL import Image
+from bitarray import bitarray
+from jagaja import Jagaja
+
+class Kodeerija:
+    """Avab pildi ja kodeerib sinna kasutaja poolt antud sisendi(d)"""
+    
+    def __init__(self, pildi_nimi: str):
+        self.fail = Image.open(pildi_nimi)
+
+        if self.fail.mode not in ["RGB", "RGBA"]:
+            raise ValueError(f"Me toetame ainult pilte, mis on RGB või RGBA formaadis, antud pilt on: {self.fail.mode}")
+
+        # pikslite arv
+        self.maht = self.fail.width * self.fail.height
+        
+        # Igal pikslil kolm kasutatavat kanalit
+        self.maht *= 3
+
+        # See indeks sisaldab nii piksli indeksit kui ka värvikanalit
+        # Värvikanali kätte saamiseks: self.indeks % 3
+        # Piksli indeksi kätte saamiseks: math.floor(self.indeks / 3)
+        self.indeks = 0
+        
+    def kodeeri(self, sisend: Jagaja):
+        if not sisend.tükkide_arv <= self.maht - self.indeks:
+            raise ValueError("Sisend on liiga suur")
+
+        # Kodeeritud info paigutus failis
+        # -------------------------------
+        # Info pikkus (tükkide arv) (16-bitine arv, 2-biti kaupa paigutatud 8-sse pikslisse)
+        # Mitu bitti on igas tükis (1-8)
+        # Tükk
+        # ...
+        # Tükk
+        # Mingi väärtus vigade kontrolli jaoks (parity check?)
