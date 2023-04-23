9e2f12d412316279f6a1ae34f6f837a3b16f71d7
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-04-23 21:09:27 +0300

    Jagaja ja Kodeerija klasside kasutamise näidis

--- mellon.py
+++ mellon.py
@@ -1,11 +1,7 @@
-from bitarray import bitarray
 from jagaja import Jagaja
+from kodeerija import Kodeerija
 
 saladus = b"Saladus!"
-
-salabitid = bitarray()
-salabitid.frombytes(bytes(saladus))
-print(salabitid.to01())
-
-tükid = list(map(bitarray.to01, Jagaja(salabitid.tobytes())))
-print("".join(tükid))
+kodeerija = Kodeerija("must.png")
+sisend = Jagaja(saladus)
+kodeerija.kodeeri_otse(sisend)
