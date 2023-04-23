c31e0048d4b7c98aa63f832f343fed33cdaaccbe
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-04-23 21:01:54 +0300

    Abifunktsioon uue piksli loomiseks

--- funktsioonid.py
+++ funktsioonid.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from bitarray import bitarray
 
 def bitideks(number: int) -> bitarray:
@@ -11,3 +12,16 @@ def bitidest(bitid: bitarray) -> int:
     """Bitarray objekt int objektiks. Kõige väiksema väärtusega bitt kõige paremal."""
 
     return int(bitid.to01(), 2)
+
+def uus_piksel(algne_px: Tuple[int, int, int], sisend: bitarray, kanal: int):
+    if kanal not in range(0, 3):
+        raise ValueError("Kanal peab olema vahemikus range(0, 3)")
+
+    algne = bitideks(algne_px[kanal])
+    uus = algne
+    uus[-2:] = False # Viimased kaks bitti nulliks, et sinna sisend asendada
+    uus |= sisend
+    # Peame alguses looma selle listina, sest tupleit pole võimalik peale loomist muuta
+    uus_px = [algne_px[0], algne_px[1], algne_px[2]]
+    uus_px[kanal] = bitidest(uus)
+    return tuple(uus_px)
