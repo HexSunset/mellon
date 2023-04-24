c01288ac95b7ac8fd43db7387bf42ee26053e56f
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-04-24 23:09:07 +0300

    koodeeri ja dekooderi näide

--- src/mellon.py
+++ src/mellon.py
@@ -1,7 +1,16 @@
 from jagaja import Jagaja
 from kooder import Kooder
+from dekooder import Dekooder
 
-saladus = b"Saladus!"
-kodeerija = Kooder("must.png")
+saladus = b"Supersalajane"
+print(f"sisend: {saladus}")
+kooder = Kooder("rott.png")
 sisend = Jagaja(saladus)
-kodeerija.kodeeri_otse(sisend)
+kooder.kodeeri_otse(sisend)
+del kooder
+
+dekooder = Dekooder("rott.png")
+väljund = dekooder.dekodeeri_otse()
+del dekooder
+
+print(f"väljund: {väljund}")
