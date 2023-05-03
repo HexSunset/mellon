87ba183c20e1cb077252dbf0b7872d271e24acf5
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-03 14:16:14 +0300

    Käsurea argumendid failide lisamiseks, saladus generic binary
    Saladuse fail võib olla ükskõik mis formaadis

--- src/mellon.py
+++ src/mellon.py
@@ -4,21 +4,22 @@ from dekooder import Dekooder
 import argparse
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("sisend_pilt", help="pilt, millesse peidetakse info")
-arg_parser.add_argument("väljund_pilt", help="uus nimi, millena infot sisaldav pilt salvestatakse")
+arg_parser.add_argument("salajane_fail", type=str, help="salajane fail, mille sisu peidetakse pilti")
+arg_parser.add_argument("sisend_pilt", type=str, help="pilt, millesse peidetakse info")
+arg_parser.add_argument("väljund_pilt", type=str, help="uus nimi, millena infot sisaldav pilt salvestatakse")
 args = arg_parser.parse_args()
+saladus = open(args.salajane_fail, "rb").read()
 sisend_pilt: str = args.sisend_pilt
 väljund_pilt: str = args.väljund_pilt
 
-saladus = b"Supersalajane"
 print(f"sisend: {saladus}")
 kooder = Kooder(sisend_pilt, väljund_pilt)
 sisend = Jagaja(saladus)
-kooder.kodeeri_otse(sisend)
+kooder.kodeeri_plokkidena(sisend)
 del kooder
 
 dekooder = Dekooder(väljund_pilt)
-väljund = dekooder.dekodeeri_otse()
+väljund = dekooder.dekodeeri_plokid()
 del dekooder
 
 print(f"väljund: {väljund}")
