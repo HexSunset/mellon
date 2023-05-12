35178b7b0106374c17c8e0feaea25ed146bf1b92
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-12 17:26:30 +0300

    Jagaja tagastab tükid uues formaadis

--- src/jagaja.py
+++ src/jagaja.py
@@ -1,6 +1,8 @@
 from bitarray import bitarray
 
 class Jagaja:
+    """Tükeldab sisendbaidid tüki_suurus biti kaupa. Annab tükid formaadis 111111XX, selles formaadis baiti saab ühildada algse baidiga & operatsiooni abil."""
+
     # TODO: toeta erinevaid sisendi variante?
     def __init__(self, sisend: bytes, tüki_suurus=2):
         self.tüki_suurus: int = tüki_suurus
@@ -20,7 +22,7 @@ class Jagaja:
         algus = self.indeks
         lõpp = self.indeks + self.tüki_suurus
         tükk = self.baidid[algus:lõpp]
-        tükk = bitarray(tükk.to01().rjust(8, '0'))
+        tükk = bitarray(tükk.to01().rjust(8, '1'))
         #print(f"algus = {algus}; lõpp = {lõpp}; tükk = {tükk.to01()}")
         self.indeks += self.tüki_suurus
         return tükk
