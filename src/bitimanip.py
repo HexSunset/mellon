14d8a27f4337652c78d04c2b2de5cadbe46f0509
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-15 20:43:26 +0300

    joonda_bitid ja bitimask funktsioonid

--- src/bitimanip.py
+++ src/bitimanip.py
@@ -10,9 +10,24 @@ def bitideks(number: int, pikkus=8) -> bitarray:
     väljund = bitarray(biti_string)
     return väljund
 
+def joonda_bitid(bitid: bitarray, pikkus=8) -> bitarray:
+    """Teeb kindlaks, et antud bitarray oleks õige pikkusega."""
+
+    if len(bitid) < pikkus:
+        bitid = bitarray(bitid.to01().rjust(pikkus, '0'))
+    if len(bitid) > pikkus:
+        bitid = bitid[-pikkus:]
+
+    return bitid
+
 def bitidest(bitid: bitarray) -> int:
     """Bitarray objekt int objektiks. Kõige väiksema väärtusega bitt kõige paremal."""
 
     return int(bitid.to01(), 2)
 
+def bitimask(maskeeritud_bitte=2) -> bitarray:
+    bitid = bitarray("11111111")
+    bitid <<= maskeeritud_bitte
+    bitid = joonda_bitid(bitid)
 
+    return bitid
