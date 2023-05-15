5d9048ca5ebd5850f1444f36a6360b6a76a34975
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-15 20:44:10 +0300

    Ploki abifunktsioonid

--- src/plokk.py
+++ src/plokk.py
@@ -1,25 +1,47 @@
-from jagaja import Jagaja
 from bitarray import bitarray
 from bitimanip import bitideks
+from typing import Tuple
 
-MAX_PLOKI_PIKKUS = 2 ** (4 * 8)
+BAIT = 8
 
 class Plokk:
-    def __init__(self, kontroll_summa: int, järjenumber: int, plokke_kokku: int, info: bytes):
-        päis = bitarray()
-        päis += bitideks(kontroll_summa, 16)
-        päis += bitideks(järjenumber)
-        päis += bitideks(plokke_kokku)
+    def __init__(self, järjenumber: int, plokke_kokku: int, baidid: bytes):
+        kodeeritud = bitarray()
 
-        self.__päis = Jagaja(päis)
-        self.__info = Jagaja(info)
+        kontroll_summa = arvuta_kontrollsumma(baidid)
+        self.__kontroll_summa = kontroll_summa
+        kodeeritud += bitideks(kontroll_summa, 2 * BAIT)
+        kodeeritud += bitideks(järjenumber)
+        self.__järjenumber = järjenumber
+        kodeeritud += bitideks(plokke_kokku)
+        self.__plokke_kokku = plokke_kokku
+        kodeeritud += bitideks(len(baidid), 4 * BAIT)
+        self.__sisu_pikkus = len(baidid)
 
-    def pikkus(self) -> int:
-        """Info pikkus koos päise pikkusega"""
+        self.__sisu = baidid
 
-        PÄISE_PIKKUS = 8
-        
-        return self.__info.pikkus + PÄISE_PIKKUS
+        kodeeritud.frombytes(baidid)
+        self.__kodeeritud = kodeeritud.tobytes()
+
+    def kontroll_summa(self) -> int:
+        return self.__kontroll_summa
+
+    def järjenumber(self) -> Tuple[int, int]:
+        return (self.__järjenumber, self.__plokke_kokku)
+
+    def sisu_pikkus(self) -> int:
+        return self.__sisu_pikkus
+
+    def kogu_pikkus(self) -> int:
+        """Terve ploki pikkus baitides."""
+
+        return len(self.__kodeeritud)
+
+    def baidid(self) -> bytes:
+        return self.__kodeeritud
+
+    def sisu_baidid(self) -> bytes:
+        return self.__sisu
 
 def arvuta_kontrollsumma(baidid: bytes) -> int:
     """Arvutab antud baitide kontrollsumma 2 baidi suuruse numbrina"""
