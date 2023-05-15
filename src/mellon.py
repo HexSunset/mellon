a08daf076836a10c3bf59386514ad57372852a8c
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-15 20:44:34 +0300

    Näidisprogramm

--- src/mellon.py
+++ src/mellon.py
@@ -1,5 +1,32 @@
-from jagaja import Jagaja
-import bitimanip as bm
+from plokk import Plokk
+from pilt import PildiHaldur
+from bitimanip import bitideks
+from bitarray import bitarray
+import sys
 
 def main():
-    pass
+    saladus = sys.argv[1]
+    seif = sys.argv[2]
+    seifi_nimi = sys.argv[3]
+    
+    baidid = None
+    with open(saladus, "rb") as f:
+        baidid = f.read()
+
+    salajane_plokk = Plokk(1, 1, baidid)
+
+    seifi_haldaja = PildiHaldur(seif)
+    seifi_haldaja.kodeeri_plokk(salajane_plokk)
+    seifi_haldaja.salvesta_pilt(seifi_nimi)
+
+    seifi_avaja = PildiHaldur(seifi_nimi)
+    plokk = seifi_avaja.dekodeeri_plokk()
+    print(f"dekodeeritud:")
+    print(f"\tkontrollsumma: {plokk.kontroll_summa()}")
+    print(f"\tploki_number: {plokk.järjenumber()[0]}/{plokk.järjenumber()[1]}")
+    print(f"\tsisu_pikkus: {plokk.sisu_pikkus()}")
+    print(f"\tploki_sisu:")
+    for bait in plokk.sisu_baidid():
+        print(f"\t\t{bitideks(bait)}")
+
+main()
